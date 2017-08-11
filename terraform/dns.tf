b90d1b1cb06619b3abf64b2e1d71914d0fd5b62d
Joshua Taylor Eppinette <josheppinette@icloud.com>
2017-08-11 12:08:05 -0400

    Remove wordpress and mysql

--- terraform/dns.tf
+++ terraform/dns.tf
@@ -6,29 +6,63 @@ resource "digitalocean_floating_ip" "root" {
 
 resource "digitalocean_domain" "root" {
   name       = "${terraform.env == "default" ? "peragrin.com" : "${terraform.env}.peragrin.com"}"
-  ip_address = "${digitalocean_floating_ip.root.ip_address}"
-  depends_on = ["digitalocean_floating_ip.root"]
+  ip_address = "198.49.23.145"
+}
+
+resource "digitalocean_record" "home-1" {
+  domain = "${digitalocean_domain.root.name}"
+  type   = "A"
+  name   = "@"
+  value  = "198.49.23.144"
+}
+
+resource "digitalocean_record" "home-2" {
+  domain = "${digitalocean_domain.root.name}"
+  type   = "A"
+  name   = "@"
+  value  = "198.185.159.145"
+}
+
+resource "digitalocean_record" "home-3" {
+  domain = "${digitalocean_domain.root.name}"
+  type   = "A"
+  name   = "@"
+  value  = "198.185.159.144"
+}
+
+resource "digitalocean_record" "www" {
+  domain = "${digitalocean_domain.root.name}"
+  type   = "CNAME"
+  name   = "www"
+  value  = "ext-cust.squarespace.com"
+}
+
+resource "digitalocean_record" "squarespace-verify" {
+  domain = "${digitalocean_domain.root.name}"
+  type   = "CNAME"
+  name   = "kjyetkf6ltnz9racpjck"
+  value  = "verify.squarespace.com"
 }
 
 resource "digitalocean_record" "api" {
   domain = "${digitalocean_domain.root.name}"
-  type   = "CNAME"
+  type   = "A"
   name   = "api"
-  value  = "@"
+  value  = "${digitalocean_floating_ip.root.ip_address}"
 }
 
 resource "digitalocean_record" "app" {
   domain = "${digitalocean_domain.root.name}"
-  type   = "CNAME"
+  type   = "A"
   name   = "app"
-  value  = "@"
+  value  = "${digitalocean_floating_ip.root.ip_address}"
 }
 
 resource "digitalocean_record" "minio" {
   domain = "${digitalocean_domain.root.name}"
-  type   = "CNAME"
+  type   = "A"
   name   = "minio"
-  value  = "@"
+  value  = "${digitalocean_floating_ip.root.ip_address}"
 }
 
 resource "digitalocean_record" "mx" {
